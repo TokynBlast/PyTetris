ea4e75aa18060f7317e309e598ce3cf39bce63d4
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-03-04 18:46:16 -0800

    Removed running python

--- run.sh
+++ run.sh
@@ -1,11 +1,7 @@
-python3
-
 if !command -v import pygame &> /dev/null; then
   echo "You need to install pygame!"
 elif !command -v import memory_profiler &> /dev/null; then
   echo "You need to install memory profiler!"
 fi
-
-exit()
 
 python3 main.py
