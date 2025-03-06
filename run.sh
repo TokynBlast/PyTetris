ac7a2591624ddcab167baaded60ddc2355487bed
Tokyn Blast <tokynblast@gmail.com>
2025-03-06 10:26:06 -0800

    Deleted run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-if !command -v import pygame &> /dev/null; then
-  echo "You need to install pygame!"
-elif !command -v import memory_profiler &> /dev/null; then
-  echo "You need to install memory profiler!"
-fi
-
-python3 main.py
