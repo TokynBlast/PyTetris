3d9383ee6b13fc70449b941dd879b8e97c893832
Tokyn Blast <tokynblast@gmail.com>
2025-03-07 10:13:47 -0800

    Removed profiler

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from src.runner import GameRunner
-from memory_profiler import profile
-
-@profile
-def main():
-    gr = GameRunner()
-    gr.game_run()
 
 if __name__=="__main__":
-    main()
+    gr = GameRunner()
+    gr.game_run()
